3e089ab93e11c8be04470a989a4d449bff0b6d02
chalanadulajdilshan <chalanadulaj98@gmail.com>
2019-07-04 12:00:17 +0530

    #22 Check first login

---
+++ ajax/js/check-login.js
@@ -0,0 +1,54 @@
+
+window.onload = function () {
+    var id = $('#id').val();
+
+    $.ajax({
+        url: "ajax/post-and-get/check-login.php",
+        type: "POST",
+        data: {
+            action: 'CHECKLOGIN',
+            id: id
+        },
+        dataType: "JSON",
+        success: function (jsonStr) {
+            
+            if (jsonStr.status == 'true') {
+
+
+                swal({
+                    title: "Are you sure?",
+                    text: "You will not be able to recover this imaginary file!",
+                    type: "warning",
+                    cancelButtonText: "Start Begginer",
+                    showCancelButton: true,
+                    confirmButtonColor: "rgb(240, 169, 0)",
+                    confirmButtonText: "Test Paper",
+
+                }, function (isConfirm) {
+                    if (isConfirm) {
+                        window.location = "test-paper.php";
+                    } else {
+                        $.ajax({
+                            url: "ajax/post-and-get/check-login.php",
+                            type: "POST",
+                            data: {
+                                action: 'UPDATESTATUS',
+                                id: id
+                            },
+                            dataType: "JSON",
+                            success: function (jsonStr) {
+                                
+                            }
+                        });
+                    }
+                });
+            }
+
+        }
+    });
+
+};
+
+
+
+
---
+++ ajax/post-and-get/check-login.php
@@ -0,0 +1,42 @@
+<?php
+
+include '../../class/include.php';
+
+if ($_POST['action'] == 'CHECKLOGIN') {
+
+    $STUDENT = new Student(NULL);
+
+    $result = $STUDENT->checkLogin($_POST['id']);
+
+    if ($result) {
+        $result['status'] = 'true';
+        echo json_encode($result);
+        header('Content-type: application/json');
+        exit();
+    } else {
+        $result['status'] = 'false';
+        echo json_encode($result);
+        header('Content-type: application/json');
+        exit();
+    }
+}
+
+//Update Status
+if ($_POST['action'] == 'UPDATESTATUS') {
+
+    $STUDENT = new Student($_POST['id']);
+    $STUDENT->status = '1';
+    $result = $STUDENT->updateStatus();
+    if ($result) {
+        $result['status'] = 'true';
+        echo json_encode($result);
+        header('Content-type: application/json');
+        exit();
+    } else {
+        $result['status'] = 'false';
+        echo json_encode($result);
+        header('Content-type: application/json');
+        exit();
+    }
+}
+?> 
