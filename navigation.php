7f706c9234e72f546394cf6cad72449b16654b92
chalanadulajdilshan <chalanadulaj98@gmail.com>
2019-07-05 11:47:57 +0530

    #39 Username and image

--- navigation.php
+++ navigation.php
@@ -40,7 +40,7 @@
                                 <button type="button" class="card-action card-toggler" title="Collapse"></button> 
 <!--                                <span class="sidenav-icon icon icon-works">&#95;</span>-->
                             </div>
-                            <strong>Lesson 01</strong>
+                            <strong>Writing</strong>
                             <span class="sidenav-icon icon icon-works">&#103;</span>
                             <div class="" style="margin-right: 50px;">
                                 <div class="progress">
@@ -112,15 +112,14 @@
                                 <button type="button" class="card-action card-toggler" title="Collapse"></button> 
                                 <span class="sidenav-icon icon icon-works ">&#95;</span>
                             </div>
-                            <strong>Lesson 02</strong>
+                            <strong>Reading</strong>
                             <span class="sidenav-icon icon icon-works">&#103;</span>
                             <div class="" style="margin-right: 50px;">
                                 <div class="progress">
                                     <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%">
                                         <span class="sr-only">0% Complete (success)</span>
                                         0%
-                                    </div>
-
+                                    </div> 
                                 </div>
                             </div>
                         </div>
@@ -183,7 +182,7 @@
                                 <button type="button" class="card-action card-toggler" title="Collapse"></button> 
                                 <span class="sidenav-icon icon icon-works">&#95;</span>
                             </div>
-                            <strong>Lesson 03</strong>
+                            <strong>Listening</strong>
                             <span class="sidenav-icon icon icon-works">&#103;</span>
                            <div class="" style="margin-right: 50px;">
                                 <div class="progress">
@@ -254,7 +253,7 @@
                                 <button type="button" class="card-action card-toggler" title="Collapse"></button> 
                                 <span class="sidenav-icon icon icon-works">&#95;</span>
                             </div>
-                            <strong>Lesson 04</strong>
+                            <strong>Speaking</strong>
                             <span class="sidenav-icon icon icon-works">&#103;</span>
                             <div class="" style="margin-right: 50px;">
                                 <div class="progress">
@@ -275,9 +274,7 @@
                                                 <a href="#"> Lesson 01</a>
                                             </h6>
                                         </div>
-
                                     </div>
-
                                 </li>
                                 <li class="list-group-item">
                                     <div class="media">
@@ -325,7 +322,7 @@
                                 <button type="button" class="card-action card-toggler" title="Collapse"></button> 
                                 <span class="sidenav-icon icon icon-works">&#95;</span>
                             </div>
-                            <strong>Lesson 01</strong>
+                            <strong>Grammar &  Vocabulary</strong>
                             <span class="sidenav-icon icon icon-works">&#103;</span>
                             <div class="" style="margin-right: 50px;">
                                 <div class="progress">
@@ -346,9 +343,7 @@
                                                 <a href="#"> Lesson 01</a>
                                             </h6>
                                         </div>
-
                                     </div>
-
                                 </li>
                                 <li class="list-group-item">
                                     <div class="media">
