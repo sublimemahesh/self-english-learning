f48ebac7a4d6ec814c6c33be52c1d71955bba660
chalanadulajdilshan <chalanadulaj98@gmail.com>
2019-07-10 12:36:16 +0530

    #76 Navigation changers

--- writing-navigation.php
+++ writing-navigation.php
@@ -18,23 +18,21 @@
                     </a>
 
 
-                   <ul class="sidenav level-2 collapse" aria-expanded="false" style="height: 0px;">
-                        <li class="sidenav-heading">Reading</li>
+                    <ul class="sidenav level-2 collapse" aria-expanded="false" style="height: 0px;"> 
                         <li class="list-group-item"><a href="lesson-home-reading-1.php">Beginner A1 reading<span class="icon1 icon-unlock-alt"></span></a></li>
                         <li class="list-group-item"><a href="#">Elementary A1 reading <span class="icon1 icon-lock"></span></a></li>
                         <li class="list-group-item"><a href="#">Elementary A2 reading <span class="icon1 icon-lock"></span></a></li>
                         <li class="list-group-item"><a href="#">Advanced C1 reading <span class="icon1 icon-lock"></span></a></li>
-                     
+
                     </ul>
                 </li>
-                 
-                  <li class="sidenav-item has-subnav open active">
 
+                <li class="sidenav-item has-subnav open active"> 
                     <a href="#" aria-haspopup="true" aria-expanded="false" class="card">
                         <span class="sidenav-icon icon icon-pencil"></span>
                         <span class="sidenav-label"><strong>Writing</strong></span>
                         <div class="">
-                           <div class="progress">
+                            <div class="progress">
                                 <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width:0%">
                                     <span class="sr-only">0% Complete (success)</span>
                                     0%
@@ -44,21 +42,20 @@
                         </div>
                     </a> 
 
-                    <ul class="sidenav level-2 collapse" aria-expanded="true">
-                        
-                        <li class="list-group-item active"><a href="lesson-home-writing.php">Lesson 01<span class="icon1 icon-unlock-alt" style="color: #f7a033;"></span></a></li>
-                        <li class="list-group-item"><a href="#">Lesson 02<span class="icon1 icon-lock"></span></a></li>
-                        <li class="list-group-item"><a href="#">Lesson 03<span class="icon1 icon-lock"></span></a></li>
-                        <li class="list-group-item"><a href="#">Lesson 04<span class="icon1 icon-lock"></span></a></li>
-                        <li class="list-group-item"><a href="#">Lesson 05<span class="icon1 icon-lock"></span></a></li> 
+                    <ul class="sidenav level-2 collapse" aria-expanded="true"> 
+                        <li class="list-group-item active"><a href="lesson-home-writing.php">Beginner A1 writing<span class="icon1 icon-unlock-alt" style="color: #f7a033;"></span></a></li>
+                        <li class="list-group-item"><a href="#">Elementary A2 writing<span class="icon1 icon-lock"></span></a></li>
+                        <li class="list-group-item"><a href="#">Intermediate B1 writing<span class="icon1 icon-lock"></span></a></li>
+                        <li class="list-group-item"><a href="#">Upper intermediate B2 writing<span class="icon1 icon-lock"></span></a></li> 
+                        <li class="list-group-item"><a href="#">Advanced C1 writing<span class="icon1 icon-lock"></span></a></li> 
                     </ul>
                 </li>
-                  <li class="sidenav-item has-subnav"> 
+                <li class="sidenav-item has-subnav"> 
                     <a href="#" aria-haspopup="true" aria-expanded="false" class="card">
                         <span class="sidenav-icon icon icon-microphone"></span>
                         <span class="sidenav-label"><strong>Listening</strong></span>
                         <div class="">
-                           <div class="progress">
+                            <div class="progress">
                                 <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width:0%">
                                     <span class="sr-only">0% Complete (success)</span>
                                     0%
@@ -68,16 +65,14 @@
                     </a>
 
 
-                   <ul class="sidenav level-2 collapse" aria-expanded="false" style="height: 0px;">
-                        <li class="sidenav-heading">Reading</li>
-                        <li class="list-group-item"><a href="lesson-home-listning.php">Lesson 01<span class="icon1 icon-unlock-alt"></span></a></li>
-                        <li class="list-group-item"><a href="#">Lesson 02<span class="icon1 icon-lock"></span></a></li>
-                        <li class="list-group-item"><a href="#">Lesson 03<span class="icon1 icon-lock"></span></a></li>
-                        <li class="list-group-item"><a href="#">Lesson 04<span class="icon1 icon-lock"></span></a></li>
-                        <li class="list-group-item"><a href="#">Lesson 05<span class="icon1 icon-lock"></span></a></li> 
+                    <ul class="sidenav level-2 collapse" aria-expanded="false" style="height: 0px;">
+                        <li class="list-group-item"><a href="lesson-home-listning.php">Beginner A1 listening<span class="icon1 icon-unlock-alt"></span></a></li>
+                        <li class="list-group-item"><a href="#">Elementary A2 speaking<span class="icon1 icon-lock"></span></a></li>
+                        <li class="list-group-item"><a href="#">Intermediate B1 speaking <span class="icon1 icon-lock"></span></a></li>
+                        <li class="list-group-item"><a href="#">Upper intermediate B2 speaking <span class="icon1 icon-lock"></span></a></li>
                     </ul>
                 </li>
-                  <li class="sidenav-item has-subnav"> 
+                <li class="sidenav-item has-subnav"> 
                     <a href="#" aria-haspopup="true" aria-expanded="false" class="card">
                         <span class="sidenav-icon icon icon-users"></span>
                         <span class="sidenav-label"><strong>Speaking</strong></span>
@@ -92,16 +87,14 @@
                     </a>
 
 
-                     <ul class="sidenav level-2 collapse" aria-expanded="false" style="height: 0px;">
-                        <li class="sidenav-heading">Reading</li>
-                        <li class="list-group-item"><a href="lesson-home-speaking.php">Lesson 01<span class="icon1 icon-unlock-alt"></span></a></li>
-                        <li class="list-group-item"><a href="#">Lesson 02<span class="icon1 icon-lock"></span></a></li>
-                        <li class="list-group-item"><a href="#">Lesson 03<span class="icon1 icon-lock"></span></a></li>
-                        <li class="list-group-item"><a href="#">Lesson 04<span class="icon1 icon-lock"></span></a></li>
-                        <li class="list-group-item"><a href="#">Lesson 05<span class="icon1 icon-lock"></span></a></li> 
+                    <ul class="sidenav level-2 collapse" aria-expanded="false" style="height: 0px;"> 
+                        <li class="list-group-item"><a href="lesson-home-speaking.php">Beginner A1 speaking<span class="icon1 icon-unlock-alt"></span></a></li>
+                        <li class="list-group-item"><a href="#">Elementary A2 speaking<span class="icon1 icon-lock"></span></a></li>
+                        <li class="list-group-item"><a href="#">Intermediate B1 speaking <span class="icon1 icon-lock"></span></a></li>
+                        <li class="list-group-item"><a href="#">Upper intermediate B2 speaking <span class="icon1 icon-lock"></span></a></li>
                     </ul>
                 </li> 
-                 <li class="sidenav-item has-subnav">
+                <li class="sidenav-item has-subnav">
 
                     <a href="#" aria-haspopup="true" aria-expanded="false" class="card">
                         <span class="sidenav-icon icon icon-language"></span>
@@ -117,13 +110,13 @@
                     </a>
 
 
-                   <ul class="sidenav level-2 collapse" aria-expanded="false" style="height: 0px;">
-                        <li class="sidenav-heading">Reading</li>
-                        <li class="list-group-item"><a href="lesson-home-grammer.php">Lesson 01<span class="icon1 icon-unlock-alt"></span></a></li>
-                        <li class="list-group-item"><a href="#">Lesson 02<span class="icon1 icon-lock"></span></a></li>
-                        <li class="list-group-item"><a href="#">Lesson 03<span class="icon1 icon-lock"></span></a></li>
-                        <li class="list-group-item"><a href="#">Lesson 04<span class="icon1 icon-lock"></span></a></li>
-                        <li class="list-group-item"><a href="#">Lesson 05<span class="icon1 icon-lock"></span></a></li> 
+                    <ul class="sidenav level-2 collapse" aria-expanded="false" style="height: 0px;">
+                        <li class="list-group-item"><a href="lesson-home-grammer.php">Adverbs of frequency<span class="icon1 icon-unlock-alt"></span></a></li>
+                        <li class="list-group-item"><a href="#">Comparative and superlative<span class="icon1 icon-lock"></span></a></li>
+                        <li class="list-group-item"><a href="#">Conjunctions<span class="icon1 icon-lock"></span></a></li>
+                        <li class="list-group-item"><a href="#">Nouns <span class="icon1 icon-lock"></span></a></li>
+                        <li class="list-group-item"><a href="#">Conditionals<span class="icon1 icon-lock"></span></a></li> 
+                        <li class="list-group-item"><a href="#">Modals of deduction<span class="icon1 icon-lock"></span></a></li> 
                     </ul>
                 </li>
             </ul>        
